a252193f984a1b7aca1c14dedf641285fb9d22a6
unknown <kkuras89@gmail.com>
2020-04-14 15:40:50 +0200

    testss

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import time
 
 print('main')
-print('Startup')
-
-while True:
-    print('1234')
-    time.sleep(2)
-print('Hallaaaaaa GIT')
+print('Startup') while True:
+    print('1234') time.sleep(2) print('Hallaaaaaa GIT')
 print('Done')
